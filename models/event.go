5cad26bef42f06627d19a1f9710d4f1ce491a099
ina-ene <ina.ene.pancake@gmail.com>
2024-08-12 11:26:07 +0300

    add createEvent

--- models/event.go
+++ models/event.go
@@ -4,9 +4,22 @@ import "time"
 
 type Event struct {
 	ID          int
-	Name        string
-	Description string
-	Location    string
-	DateTime    time.Time
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
 	UserID      int
+}
+
+var events = []Event{}
+
+func (e Event) Save() {
+	//later: add it to a database
+	events = append(events, e)
+
+}
+
+func GetAllEvents() []Event {
+	return events
+
 }
