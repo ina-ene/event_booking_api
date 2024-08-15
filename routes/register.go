59713f40f879ec39bd39bfc3ce8a26947920e2ae
ina-ene <ina.ene.pancake@gmail.com>
2024-08-15 16:48:31 +0300

    add cancel registration

--- routes/register.go
+++ routes/register.go
@@ -36,5 +36,28 @@ func registerForEvent(context *gin.Context) {
 }
 
 func cancelRegistration(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+
+	err = event.DeleteRegistration(userId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not delete registration for event."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Successfuly deleted registration!"})
 
 }
